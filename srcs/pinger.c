7bbdadf2bf080c76466aa4e6b38dfa9bb34c0505
abombard <arthbab@gmail.com>
2017-05-10 16:50:06 +0200

    icmp not ofund

--- srcs/pinger.c
+++ srcs/pinger.c
@@ -12,13 +12,21 @@
 
 #include "ping.h"
 
-static void			icmp_init(struct icmphdr *icmp)
+static int			icmp_init(struct icmphdr *icmp)
 {
+	int		packlen;
+
 	icmp->type = ICMP_ECHO;
 	icmp->code = 0;
 	icmp->checksum = 0;
 	icmp->un.echo.sequence = ++g_context.ntransmitted;
 	icmp->un.echo.id = g_context.ident;
+	(void)gettimeofday((struct timeval *)(icmp + 1),
+		(struct timezone *)0);
+	packlen = DATALEN + 8;
+	icmp->checksum = compute_checksum((void *)packet, packlen);
+	g_context.recv_table[icmp->un.echo.sequence % MAX_DUP_CK] = 0;
+	return (packlen);
 }
 
 void				pinger(void)
@@ -29,12 +37,7 @@ void				pinger(void)
 	int				nbytes;
 
 	ft_bzero(packet, MAXPACKET);
-	icmp_init((struct icmphdr *)packet);
-	(void)gettimeofday((struct timeval *)(packet + sizeof(struct icmphdr)),
-		(struct timezone *)0);
-	packlen = DATALEN + 8;
-	icmp->checksum = compute_checksum((void *)packet, packlen);
-	g_context.recv_table[icmp->un.echo.sequence % MAX_DUP_CK] = 0;
+	packlen = icmp_init((struct icmphdr *)packet);
 	i = 0;
 	while (i < packlen)
 	{
